85013c91266a31a0e8079d1cbb3026259c94f8f9
jonnybrazil <2.92964363e+08+jonnybrazil@users.noreply.github.com>
2026-09-25 19:00:33 +0000

    Refresh main page branding to GOSPELLAB

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'radio-gospel-lab-v4';
+const CACHE_NAME = 'radio-gospel-lab-v5';
 const APP_SHELL = ['.', './index.html', './styles.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', event => {
