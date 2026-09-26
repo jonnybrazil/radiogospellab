3a8d5f5fd7ba75c0c9a0953508aa90787d27dd71
jonnybrazil <2.92964363e+08+jonnybrazil@users.noreply.github.com>
2026-09-26 10:33:47 +0000

    Remove programming section divider

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'radio-gospel-lab-v6';
+const CACHE_NAME = 'radio-gospel-lab-v7';
 const APP_SHELL = ['.', './index.html', './styles.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', event => {
